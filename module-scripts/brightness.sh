e47cd597a21fb53ece57d8accb8de14cb3826946
Patrick Gordon <pgpatrickgordon@gmail.com>
2021-03-11 19:48:24 +0000

    add full brightness control

--- module-scripts/brightness.sh
+++ module-scripts/brightness.sh
@@ -1,4 +1,15 @@
 set -euo pipefail
 
 brightness=$(xrandr --verbose | awk '/Brightness/ { print $2 * 100 }' | head -1)
-echo "brightness $brightness%"
+
+case ${1-none} in
+    up)
+        xrandr --output eDP-1 --brightness $(awk "BEGIN { print ($brightness + 1) / 100 }")
+        ;;
+    down)
+        xrandr --output eDP-1 --brightness $(awk "BEGIN { print ($brightness - 1) / 100 }")
+        ;;
+    none)
+        echo "brightness $brightness%"
+        ;;
+esac
